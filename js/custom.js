bc7f95b04d83b234bd377b5f835e201c8575b5c4
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-08-25 13:06:42 +0600

    slider

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$('.banner-slider').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 1,
+    infinite: true,
+    nextArrow: '<i class="fas fa-caret-circle-right"></i>',
+    prevArrow: '<i class="fas fa-caret-circle-left"></i>',
+});
