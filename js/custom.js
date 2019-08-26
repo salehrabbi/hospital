4f77bd59794b553b635241f64aff2d485d6b7db4
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-08-26 12:33:07 +0600

    yesterday

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,29 @@
 $('.banner-slider').slick({
     dots: false,
-    infinite: false,
     slidesToShow: 1,
     infinite: true,
     nextArrow: '<i class="fas fa-caret-circle-right"></i>',
     prevArrow: '<i class="fas fa-caret-circle-left"></i>',
+    draggable: true,
+    autoplay: true,
+});
+
+$('.evento-slider').slick({
+    dots: false,
+    slidesToShow: 1,
+    infinite: true,
+    nextArrow: '<i class="fas fa-caret-circle-right"></i>',
+    prevArrow: '<i class="fas fa-caret-circle-left"></i>',
+    draggable: true,
+    autoplay: true,
+});
+
+$('.patner-slider').slick({
+    dots: false,
+    slidesToShow: 3,
+    infinite: true,
+    nextArrow: '<i class="fas fa-caret-circle-right"></i>',
+    prevArrow: '<i class="fas fa-caret-circle-left"></i>',
+    draggable: true,
+    autoplay: true,
 });
