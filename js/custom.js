c53644a93a04f1af22f8cada034eeef3c691e5ef
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-08-28 11:47:57 +0600

    tody

--- js/custom.js
+++ js/custom.js
@@ -26,4 +26,96 @@ $('.patner-slider').slick({
     prevArrow: '<i class="fas fa-caret-circle-left"></i>',
     draggable: true,
     autoplay: true,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+    },
+  ]
 });
+
+
+
+$(document).on('click', '.yamm .dropdown-menu', function (e) {
+    e.stopPropagation()
+});
+
+
+
+$(function () {
+    $(".options i").click(function () {
+        $(".options").toggleClass("active");
+    });
+});
+
+$(function () {
+    $(".options .search").click(function () {
+        $(".search-form").addClass("active");
+    });
+});
+
+$(function () {
+    $(".options .share").click(function () {
+        $(".share-area").addClass("active");
+    });
+});
+
+$(function () {
+    $(".options .setting").click(function () {
+        $(".setting-area").addClass("active");
+    });
+});
+
+
+$(function () {
+    $(".options .hide-item").click(function () {
+        $(".option-area").removeClass("active");
+    });
+});
+
+
+//var contraste = 0;
+function changeFilter(contraste) {
+    switch (contraste) {
+        case 0:
+            document.getElementById("page-top").style.filter = "grayscale(100%)";
+            //contraste 0;
+            break;
+        case 1:
+            document.getElementById("page-top").style.filter = "hue-rotate(90deg)";
+            //contraste 0;
+            break;
+        case 2:
+            document.getElementById("page-top").style.filter = "invert(200%)";
+            // contraste ++;
+            break;
+        case 3:
+            document.getElementById("page-top").style.filter = "none";
+            // contraste = 0;
+            break;
+    }
+};
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+var size = 16;
+$(document).ready(function () {
+    $("#btn-increase").click(function () {
+        $("html").css("font-size", size + 1 + "px");
+        size++;
+    });
+    $("#btn-decrease").click(function () {
+        $("html").css("font-size", size - 1 + "px");
+        size--;
+    });
+});
+
+
+
+$(".header-space").height($(".main-header").outerHeight(true));
